2e5aad23dc9cc5d6bd8a878eaafee2a37b49fd78
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-06-24 16:31:36 -0500

    stack views

---
+++ warCardGame/warCardGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  warCardGame
+//
+//  Created by Keith Barrett Tomlinson on 6/22/17.
+//  Copyright © 2017 Keith Barrett Tomlinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
