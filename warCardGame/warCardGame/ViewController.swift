a42a81711fe4ca316ed795c7ddfbd170346fa24c
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-07-06 11:41:43 -0400

    lesson 17 completes war game

--- warCardGame/warCardGame/ViewController.swift
+++ warCardGame/warCardGame/ViewController.swift
@@ -11,11 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var leftCardView: UIImageView!
-    @IBOutlet weak var rightCardView: UIImageView!
     @IBOutlet weak var leftScoreView: UILabel!
-    @IBOutlet weak var rightScoreView: UILabel!
+    var leftScore = 0
     
-    let cardNames = ["ace", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king"]
+    @IBOutlet weak var rightCardView: UIImageView!
+    @IBOutlet weak var rightScoreView: UILabel!
+    var rightScore = 0
+    
+    let cardNames = ["card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king", "ace"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,13 +45,21 @@ class ViewController: UIViewController {
 
         //compare the card numbers
         if leftNumber > rightNumber{
-            //left wins
+            //left wins incriment score
+            leftScore += 1
+            
+            //update Label
+            leftScoreView.text = String(leftScore)
         }
         else if leftNumber == rightNumber{
             //its a tie
         }
         else {
-            //right wins
+            //right wins incriment score
+            rightScore += 1
+            
+            //update Label
+            rightScoreView.text = String(rightScore)
         }
         
     }
